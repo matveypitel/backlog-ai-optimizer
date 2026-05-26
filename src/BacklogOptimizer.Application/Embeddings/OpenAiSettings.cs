c8cbdcf5a98aefe92c1f7e1455f60954549fa9f6
Matvey Pitel <matveypitel06@gmail.com>
2026-05-26 22:10:20 +0500

    Update open ai settings for new threshholds

--- src/BacklogOptimizer.Application/Embeddings/OpenAiSettings.cs
+++ src/BacklogOptimizer.Application/Embeddings/OpenAiSettings.cs
@@ -8,9 +8,9 @@ public class OpenAiSettings
     public string EmbeddingModel { get; init; } = "text-embedding-3-small";
     public string[] CompletedStatuses { get; init; } = ["Done", "Closed", "Resolved"];
     public string CompletionModel { get; init; } = "gpt-4o-mini";
-    public double CoverageGapThreshold { get; init; } = 0.7;
-    public int ReprioritizationSimilarFeaturesTopN { get; init; } = 3;
-    public double ReprioritizationSimilarityThreshold { get; init; } = 0.3;
+    public double CoverageGapThreshold { get; init; } = 0.55;
+    public int ReprioritizationSimilarFeaturesTopN { get; init; } = 10;
+    public double ReprioritizationSimilarityThreshold { get; init; } = 0.68;
     public int FeatureSuggestionBatchSize { get; init; } = 15;
     public int ReprioritizationBatchSize { get; init; } = 15;
 }
